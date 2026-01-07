04bead831794a078703cbcd47ad1c6cd78451787
harisha <harsha2002jc@gmail.com>
2026-01-07 17:50:23 +0530

    car/page.js  file added for seo

---
+++ app/car/page.js
@@ -0,0 +1,10 @@
+export const dynamic = "force-static";
+
+export default function CarsPage() {
+  return (
+    <main>
+      <h1>Available Cars</h1>
+      <p>Browse and book cars available on MM Miles.</p>
+    </main>
+  );
+}
